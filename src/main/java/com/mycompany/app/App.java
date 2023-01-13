d3d6e9ea8ac39782b91d9c51feb5c3bbab02b0d2
Osla Mbakaem <oslalekeaka@gmail.com>
2023-01-12 21:41:02 -0500

    Updated App.java1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World From AWS & JENKINS !" );
+        System.out.println( "Hello World From AWS & JENKINS !!!!" );
     }
 }
